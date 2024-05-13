407e5d22bb2f1065fe75d6f70c07043912834bda
student9128 <jingpengchn@163.com>
2024-05-13 12:39:06 +0800

    add:add some pages & colors

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -14,14 +14,56 @@ export default class EntryAbility extends UIAbility {
   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
-
-    windowStage.loadContent('pages/Index', (err, data) => {
-      if (err.code) {
-        hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
-        return;
+    windowStage.getMainWindow((err,windowClass)=>{
+      if(err.code){
+        hilog.info(0x0000, 'testTag', '%{public}s', JSON.stringify(err));
       }
-      hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
-    });
+      // //状态栏、导航栏的属性
+      // var SystemBarProperties = {
+      //   statusBarColor: '#2196f3', // 状态栏背景色透明
+      //   //navigationBarColor: '#00ff00',
+      //   // 以下两个属性从API Version7开始支持
+      //   isStatusBarLightIcon: false,
+      //   //isNavigationBarLightIcon: false,
+      //   // 以下两个属性从API Version8开始支持
+      //   statusBarContentColor: '#ffff00',
+      //   navigationBarContentColor: '#000000'
+      // };
+      // windowClass.setSystemBarProperties(SystemBarProperties,(err)=>{
+      //   console.log("Hello",JSON.stringify(err))
+      // })
+      // windowClass.setWindowLayoutFullScreen(true,(error,data)=>{
+      //   if(err.code==0){
+      //     hilog.info(0x0000, 'testTag', '%{public}s', '设置成功');
+      //   }else {
+      //     hilog.info(0x0000, 'testTag', '%{public}s', '设置失败');
+      //   }
+      // })
+      // windowClass.setWindowSystemBarEnable(["navigation"])
+      // windowClass.setWindowSystemBarProperties({statusBarColor:'#2196f3',  statusBarContentColor:'#FF5580'},(err)=>{
+      //   console.log(`${JSON.stringify(err)}`);
+      // })
+      windowClass.setUIContent('pages/Index',(err)=>{
+        if(err.code==0){
+
+      let color: string = '#eeeeee';
+      try {
+        windowClass.setWindowBackgroundColor(color);
+      } catch (exception) {
+        console.error('Failed to set the background color. Cause: ' + JSON.stringify(exception));
+      };
+        }
+      })
+      // windowClass.setWindowBackgroundColor('#2196f3')
+    })
+
+    // windowStage.loadContent('pages/Index', (err, data) => {
+    //   if (err.code) {
+    //     hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+    //     return;
+    //   }
+    //   hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+    // });
   }
 
   onWindowStageDestroy() {
